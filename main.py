e20cfd9b0e85ff710e22dc280e2866de75550c31
marceloyuzo <marceloyuzo0@hotmail.com>
2025-01-17 20:30:59 -0300

    sistema quase pronto

--- main.py
+++ main.py
@@ -2,102 +2,141 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class Objeto(): 
-  def __init__(self):
-    self.NV = 5
-    self.matriz = np.array([[1, 7, 7, 1, 4], [1, 1, 1, 1, 7], [1, 1, 7, 7, 4], [1, 1, 1, 1, 1]], dtype=float)
-    self.NS = 5
-    self.NVPS = np.array([4, 3, 3, 3, 3], dtype=float)
-    self.VS = None
-
+   def __init__(self):
+      self.NV = 5
+      self.matriz = np.array([[1, 7, 7, 1, 4], [1, 1, 1, 1, 7], [1, 1, 7, 7, 4], [1, 1, 1, 1, 1]], dtype=float)
+      self.NS = 5
+      self.NVPS = np.array([4, 3, 3, 3, 3], dtype=float)
+      self.VS = [[4, 3, 5, 4], [3, 2, 5, 3], [5, 2, 1, 5], [4, 5, 1, 4], [4, 1, 2, 3, 4]]
 
 def calcularVetorNormal(pontoA, pontoB, pontoC):
-  vetorAB = np.array(pontoB, dtype=float) - np.array(pontoA, dtype=float)
-  vetorCB = np.array(pontoC, dtype=float) - np.array(pontoB, dtype=float)
+   vetorAB = np.array(pontoB, dtype=float) - np.array(pontoA, dtype=float)
+   vetorCB = np.array(pontoC, dtype=float) - np.array(pontoB, dtype=float)
 
-  vetorNormal = np.cross(vetorAB, vetorCB)
+   vetorNormal = np.cross(vetorAB, vetorCB)
 
-  return vetorNormal
+   return vetorNormal
 
 def calcularMatrizPerspectiva(pontoVista, vetorNormal, pontoPlano):
-  # d0 depende do plano e de um ponto sobre o plano
-  d0 = np.dot(pontoPlano, vetorNormal)
+   # d0 depende do plano e de um ponto sobre o plano
+   d0 = np.dot(pontoPlano, vetorNormal)
 
-  # d1 depende do plano e do ponto de vista
-  d1 = np.dot(pontoVista, vetorNormal)
+   # d1 depende do plano e do ponto de vista
+   d1 = np.dot(pontoVista, vetorNormal)
 
-  d = d0 - d1
+   d = d0 - d1
 
-  matrizPerspectiva = [
-    [(d + pontoVista[0] * vetorNormal[0]), (pontoVista[0] * vetorNormal[1]), (pontoVista[0] * vetorNormal[2]), (-pontoVista[0] * d0)], 
-    [(pontoVista[1] * vetorNormal[0]), (d + pontoVista[1] * vetorNormal[1]), (pontoVista[1] * vetorNormal[2]), (-pontoVista[1] * d0)], 
-    [(pontoVista[2] * vetorNormal[0]), (pontoVista[2] * vetorNormal[1]), (d + pontoVista[2] * vetorNormal[2]), (-pontoVista[2] * d0)], 
-    [(vetorNormal[0]), (vetorNormal[1]), (vetorNormal[2]), (-d1)]
-  ]
+   matrizPerspectiva = [
+      [(d + pontoVista[0] * vetorNormal[0]), (pontoVista[0] * vetorNormal[1]), (pontoVista[0] * vetorNormal[2]), (-pontoVista[0] * d0)], 
+      [(pontoVista[1] * vetorNormal[0]), (d + pontoVista[1] * vetorNormal[1]), (pontoVista[1] * vetorNormal[2]), (-pontoVista[1] * d0)], 
+      [(pontoVista[2] * vetorNormal[0]), (pontoVista[2] * vetorNormal[1]), (d + pontoVista[2] * vetorNormal[2]), (-pontoVista[2] * d0)], 
+      [(vetorNormal[0]), (vetorNormal[1]), (vetorNormal[2]), (-d1)]
+   ]
 
-  return np.array(matrizPerspectiva)
+   return np.array(matrizPerspectiva)
 
 def homogeneasParaCartesianasDoMundo(matrizHomogenea):
-  qtdVertices = len(matrizHomogenea[0])
+   qtdVertices = len(matrizHomogenea[0])
 
-  for i in range(qtdVertices):
-    quartoElemento = matrizHomogenea[-1][i]
+   for i in range(qtdVertices):
+      quartoElemento = matrizHomogenea[-1][i]
 
-    for linha in matrizHomogenea:
-      teste = linha[i] / quartoElemento
+      for linha in matrizHomogenea:
+         teste = linha[i] / quartoElemento
 
-      linha[i] = teste
-      
-  return matrizHomogenea[:2]
+         linha[i] = teste
+
+   matrizCartesianaDoMundo = matrizHomogenea[:2]
+   matrizCartesianaDoMundo[1] = -matrizCartesianaDoMundo[1]
+         
+   return matrizCartesianaDoMundo
 
 def janelaParaViewport(matrizCartesianaDoMundo, janela, viewport):
-    # Limites da janela (mundo)
-    x_wmin, y_wmin, x_wmax, y_wmax = janela
+   # Limites da janela (mundo)
+   x_min, y_min, x_max, y_max = janela
 
-    # Limites da viewport (tela)
-    x_vmin, y_vmin, x_vmax, y_vmax = viewport
+   aspectRatioJanela = (x_max - x_min) / (y_max - y_min)
 
-    # Convertendo cada ponto
-    matrizViewport = []
-    for x, y in zip(matrizCartesianaDoMundo[0], matrizCartesianaDoMundo[1]):
-        x_v = x_vmin + ((x - x_wmin) / (x_wmax - x_wmin)) * (x_vmax - x_vmin)
-        y_v = y_vmin + ((y - y_wmin) / (y_wmax - y_wmin)) * (y_vmax - y_vmin)
-        matrizViewport.append([x_v, y_v])
+   # Limites da viewport (tela)
+   u_min, v_min, u_max, v_max = viewport
+
+   aspectRatioViewport = (u_max - u_min) / (v_max - v_min)
+
+   matrizViewport = []
+   Sx = (u_max - u_min) / (x_max - x_min)
+   Sy = (v_max - v_min) / (y_max - y_min)
+
+   if(aspectRatioJanela > aspectRatioViewport):
+      v_maxNovo = ((u_max - u_min) / aspectRatioJanela) + v_min
+
+      matrizViewport = [[Sx, 0, u_min - (Sx * x_min)], [0, -Sy, (Sy * y_max) + (v_max / 2) - (v_maxNovo / 2) + v_min], [0, 0, 1]]
+   
+   if(aspectRatioJanela < aspectRatioViewport):
+      u_maxNovo = aspectRatioJanela * (v_max - v_min) + u_min
+
+      matrizViewport = [[Sx, 0, (-Sx * x_min) + (u_max / 2) - (u_maxNovo / 2) + u_min], [0, -Sy, (Sy * y_max) + v_min], [0, 0, 1]]
+
+   if(aspectRatioJanela == aspectRatioViewport):
+      matrizViewport = [[Sx, 0, u_min - (Sx * x_min)], [0, -Sy, (Sy * y_max) + v_min ], [0, 0, 1]]
+
+   matrizViewport = np.array(matrizViewport, dtype=float)
+
+   novaLinha = np.ones(matrizCartesianaDoMundo.shape[1])
+   matrizCartesianaDoMundoAtualizada = np.vstack([matrizCartesianaDoMundo, novaLinha])
+
+   return np.dot(matrizViewport, matrizCartesianaDoMundoAtualizada)
+
+
+def plotarObjetoComArestas(matrizViewport, conexoes):
+    plt.figure(figsize=(8, 6))
     
-    return np.array(matrizViewport).T  # Transpor para manter a estrutura original
-
-# def plotarObjeto(matrizViewport):
-#     x = matrizViewport[0]
-#     y = matrizViewport[1]
+    # Separar as coordenadas X e Y dos vértices
+    x_coords = matrizViewport[0]
+    y_coords = matrizViewport[1]
     
-#     plt.figure(figsize=(6, 6))
-#     plt.scatter(x, y, color='blue')  # Plota os pontos
-#     plt.plot(x, y, color='red')      # Plota as linhas conectando os pontos
-#     plt.title("Projeção do Objeto na Viewport")
-#     plt.xlabel("X")
-#     plt.ylabel("Y")
-#     plt.grid()
-#     plt.show()
+    # Plotar os vértices
+    plt.scatter(x_coords, y_coords, color='blue', label='Vértices')
+    
+    # Plotar as arestas
+    for aresta in conexoes:
+        for i in range(len(aresta) - 1):
+            x = [x_coords[aresta[i] - 1], x_coords[aresta[i + 1] - 1]]
+            y = [y_coords[aresta[i] - 1], y_coords[aresta[i + 1] - 1]]
+            plt.plot(x, y, color='black', linewidth=1)
 
+    # Configurações do gráfico
+    plt.title("Objeto no Sistema Viewport")
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.grid(True)
+    plt.axis("equal")
+    plt.legend()
+    plt.show()
 
 def main():
-  objeto = Objeto()
-  pontoVista = [20, 10, 30]
+   objeto = Objeto()
 
-  # Vetor normal ao plano Z = 0
-  vetorNormal = np.array([0, 0, 1], dtype=float)
+   # INPUT DE ENTRADA
+   pontoVista = [20, 10, 30]
 
-  # Ponto sobre o plano Z = 0
-  pontoPlano = np.array([0, 0, 0], dtype=float)
+   # Vetor normal ao plano Z = 0 (FIXO)
+   vetorNormal = np.array([0, 0, 1], dtype=float)
 
-  matrizPerspectiva = calcularMatrizPerspectiva(pontoVista, vetorNormal, pontoPlano)
+   # Ponto sobre o plano Z = 0 (FIXO)
+   pontoPlano = np.array([0, 0, 0], dtype=float)
 
-  matrizLinhaObjeto = np.dot(matrizPerspectiva, objeto.matriz)
-  
-  matrizCartesianaDoMundo = homogeneasParaCartesianasDoMundo(matrizLinhaObjeto)
+   matrizPerspectiva = calcularMatrizPerspectiva(pontoVista, vetorNormal, pontoPlano)
 
-  # Transformação janela-viewport
-  janela = [-10, -10, 10, 10]  # Limites do mundo (ajuste conforme necessário)
-  viewport = [0, 0, 500, 500]  # Resolução da tela (500x500 px)
-  matrizViewport = janelaParaViewport(matrizCartesianaDoMundo, janela, viewport)
+   matrizLinhaObjeto = np.dot(matrizPerspectiva, objeto.matriz)
+   
+   matrizCartesianaDoMundo = homogeneasParaCartesianasDoMundo(matrizLinhaObjeto)
+
+   # Transformação janela-viewport
+   janela = [-7, -5, 9, 7]  # Limites do mundo (ajuste conforme necessário)
+   viewport = [0, 0, 32, 24]  # Resolução do dispositivo 32px x 24px
+   matrizJanelaViewport = janelaParaViewport(matrizCartesianaDoMundo, janela, viewport)
+   print(matrizJanelaViewport)
+
+   plotarObjetoComArestas(matrizJanelaViewport, objeto.VS)
 
 main()
